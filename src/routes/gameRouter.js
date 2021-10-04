a7099ae38b6e443f07a237ec147079da784d64e5
Lilian Hiault <lilian.hiault@laposte.net>
2021-10-04 17:49:46 +0200

    Création de partie

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -5,14 +5,13 @@ const router = express.Router()
 
 // Listen to POST /games
 router.post("/", function (req, res) {
-    // TODO return 400 if req.body.name doesn't exist
-    try {
-        const newGame = gameService.createGame(req.body.name)
-        res.status(201).json(newGame)
-    }
-    catch (err) {
-        res.status(400)
-    }
+  // TODO return 400 if req.body.name doesn't exist
+  try {
+    const newGame = gameService.createGame(req.body.name)
+    res.status(201).json(newGame)
+  } catch (err) {
+    res.status(400).send("Missing name parameter")
+  }
 })
 
 export default router
