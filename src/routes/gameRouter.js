73742e52d278c0eba128096f22dbcfde9cd5d5fa
Lilian Hiault <lilian.hiault@laposte.net>
2021-10-18 18:57:22 +0200

    Request take-good response

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 // Listen to POST /games
 router.post("/", function (req, res) {
+  // Return 400 if req.body.name doesn't exist
   try {
     const newGame = gameService.createGame(req.body.name)
     res.status(201).json(newGame)
@@ -61,5 +62,30 @@ router.delete("/:id", function (req, res) {
   }
 })
 */
+router.put("/:id/take-good", function (req, res) {
+  /* Return 200 if player can take good
+    403 if he has 7 cards or if it's not his turn
+    404 if the game id doesn't exist
+  */
+  const games = databaseService.getGames()
+  const currGame = games.find((game) => game.id === req.body.id)
+  if (currGame) {
+    if (currGame.currentPlayerIndex === req.body.playerIndex) {
+      if (currGame._players[req.body.playerIndex].hand.length < 8) {
+        gameService.takeGood(
+          req.body.id,
+          req.body.playerIndex,
+          req.body.takeGoodPayload
+        )
+      } else {
+        req.status(403).send("The hand's size can't exceed 7 cards")
+      }
+    } else {
+      res.status(403).send("It's not this player's turn")
+    }
+  } else {
+    res.status(404).send("Invalid game id")
+  }
+})
 
 export default router
